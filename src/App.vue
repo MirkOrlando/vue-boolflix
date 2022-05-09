4e5144553b21b4b89cbbb4d1c015c76a598a19b8
MirkOrlando <mirko@orlando-m.it>
2022-05-09 11:20:48 +0200

    created base structure in app

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,18 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <header>Site Header Here</header>
+    <main>Site Main Here</main>
+    <footer>Site Footer Here</footer>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: {},
+};
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+@import "@/assets/scss/style.scss";
 </style>
